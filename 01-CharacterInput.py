ec737822583b15f8fbb973676091e86a1a272aa8
Franco Sosuan <franco.sosuan@gmail.com>
2019-02-05 19:43:25 +0800

    Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.

--- 01-CharacterInput.py
+++ 01-CharacterInput.py
@@ -1,1 +1,10 @@
-print("Test Connection")
+name = str(input("Please enter your name: "))
+age = int(input("Please enter your age: "))
+times = int(input("Please enter number of times of message: "))
+
+time_age = 100 - age
+
+for message in range(0,times):
+    print("Hi " + name + ", you will turn 100 after {0} years" .format(time_age))
+
+
