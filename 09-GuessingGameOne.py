80e12fa4c70ca1128f3ca51071b13c1ca55456d9
Franco Sosuan <franco.sosuan@gmail.com>
2019-02-06 14:59:00 +0800

    Generate a random number between 1 and 9 (including 1 and 9). Ask the user to guess the number, then tell them whether they guessed too low, too high, or exactly right. (Hint: remember to use the user input lessons from the very first exercise)

--- 09-GuessingGameOne.py
+++ 09-GuessingGameOne.py
@@ -7,11 +7,13 @@ while True:
     guess = int(input("Please guess the number from 1 to 9: "))
     counter = counter + 1
 
-    if num == guess:
+    if num > guess:
+        print("Too Low!")
+    elif num < guess:
+        print("Too High!")
+    else:
         print("Congratulations! You have guessed the number! ")
         break
-    else:
-        print("Guess again: ")
 
 
 print("Number of guesses: {0}" .format(counter))
