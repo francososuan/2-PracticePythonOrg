4dd126347330e6000ba0fabae1b18f7fcfb8d4df
Franco Sosuan <franco.sosuan@gmail.com>
2019-02-06 13:20:01 +0800

    Let’s say I give you a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]. Write one line of Python that takes this list a and makes a new list that has only the even elements of this list in it.

---
+++ 07-ListComprehensions.py
@@ -0,0 +1,8 @@
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+even_a = [number for number in a if number %2 ==0]
+
+#for number in a:
+#    if number %2 ==0:
+#        even_a.append(number)
+
+print(even_a)
